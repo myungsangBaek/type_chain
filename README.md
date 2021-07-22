ec83231982001b689f0fdb4421539f1fb5977f90
myungsangBaek <bms6168@naver.com>
2021-07-22 15:56:52 +0900

    Setting

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # type_chain
 I made a blockchain with TypeScript!
+
+Learning Typescript by making a Blockchain with it.
---
+++ index.ts
@@ -0,0 +1,11 @@
+const name = "Han",
+    age = 27,
+    gender = "male";
+
+const sayHi = (name, age, gender?) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+
+sayHi(name, age);
+
+export {};
